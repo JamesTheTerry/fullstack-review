3fc31f2feb7afe881063f7334d273be947c14875
James Terry <jamesnorthterry@gmail.com>
2017-11-08 13:14:40 -0800

    Init and defined schema

--- database/index.js
+++ database/index.js
@@ -2,15 +2,28 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Schema.ObjectId,
+  name: String,
+  url: String,
+  stars: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let save = (data) => {
+  // take the data
+  var formatted = {
+    name: data.name,
+    url: data.html_url,
+    stars: data.stargazers_count
+    username: data.owner.login,
+    userid: data.owner.id
+  }
+
+  Repo.create(formatted, function (err, responseMaybe) {
+    if (err) return handleError(err);
+    // saved!
+  })
 }
 
 module.exports.save = save;

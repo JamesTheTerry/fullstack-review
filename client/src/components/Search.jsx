f4c614ba1bbe75a8a9253dd2bec16ac2ffeb3e74
James Terry <jamesnorthterry@gmail.com>
2017-11-08 14:06:07 -0800

    Fixed the search bug and added a return key watcher in the input so I dont have to click the button

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -6,6 +6,16 @@ class Search extends React.Component {
     this.state = {
       term: ''
     }
+    this.onKeyDown = this.onKeyDown.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  onKeyDown (e) {
+    if (e.keyCode === 13) {
+      console.log('Enter');
+      this.search();
+    }
   }
 
   onChange (e) {
@@ -21,9 +31,9 @@ class Search extends React.Component {
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>       
+      Enter a github username: <input value={this.state.terms} onChange={this.onChange} onKeyDown={this.onKeyDown}/>
       <button onClick={this.search}> Add Repos </button>
-    </div>) 
+    </div>)
   }
 }
 

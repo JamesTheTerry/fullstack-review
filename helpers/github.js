ed56e07d623f376c472d4830dcea2843a3a48d52
James Terry <jamesnorthterry@gmail.com>
2017-11-09 13:56:11 -0800

    switched to environment variable for github token

--- helpers/github.js
+++ helpers/github.js
@@ -1,5 +1,5 @@
 const request = require('request');
-const config = require('../config.js');
+// const config = require('../config.js');
 const Promise = require("bluebird");
 
 let getReposByUsername = (username, outsideCallback) => {
@@ -10,7 +10,7 @@ let getReposByUsername = (username, outsideCallback) => {
       url: `https://api.github.com/users/${username}/repos`,
       headers: {
         'User-Agent': 'request',
-        'Authorization': `token ${config.TOKEN}`
+        'Authorization': `token ${GITHUBTOKEN}`
       }
     };
 

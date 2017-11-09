f5dc1596c3ff5d188dcd4a5553ad70ca14f8790d
James Terry <jamesnorthterry@gmail.com>
2017-11-09 11:55:47 -0800

    Completed minimum but has a max 25 repo

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -4,6 +4,17 @@ const RepoList = (props) => (
   <div>
     <h4> Repo List Component </h4>
     There are {props.repos.length} repos.
+      {props.repos.map(repo => {
+        return <div className='repoSuper'>
+          <p className='title'><a href='{repo.url}'>{repo.name}</a></p>
+          <p className='stars'>{repo.stars} stars</p>
+        </div>
+
+        // return <a href='https://www.google.com'><div className='repoSuper'>
+        //   <p className='title'>jtek-chat</p>
+        //   <p className='stars'>0 stars</p>
+        // </div></a>
+      })}
   </div>
 )
 

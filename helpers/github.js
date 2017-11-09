34b9868ffb394d4d7b9ecaf660dcc80491592228
James Terry <jamesnorthterry@gmail.com>
2017-11-09 15:11:46 -0800

    the better branch

--- helpers/github.js
+++ helpers/github.js
@@ -10,7 +10,7 @@ let getReposByUsername = (username, outsideCallback) => {
       url: `https://api.github.com/users/${username}/repos`,
       headers: {
         'User-Agent': 'request',
-        'Authorization': `token ${config.TOKEN}`
+        'Authorization': `token ${process.env.GITHUBTOKEN}`
       }
     };
 
